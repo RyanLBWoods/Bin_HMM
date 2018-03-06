9d69bc63df0ff60487d6fb7dd167b1889a1ef2f7
Lin Bin <linbin@8afbcf58.cs.st-andrews.ac.uk>
2018-03-06 15:32:55 +0000

    Apply data processing

--- Test.py
+++ Test.py
@@ -2,16 +2,22 @@ import HMM
 import nltk
 from nltk.corpus import brown
 
-# Get test set
-sents = brown.tagged_sents(tagset = 'universal')
-test_length = len(sents) - HMM.train_length + 1
-print test_length
-test_sents = []
 
+# Pre-processing training and testing sets
+sents = brown.tagged_sents(tagset='universal')  # Get training set
+# train_length = len(sents) / 2  # Define size of training set
+train_length = 100
+training_sents = []
+test_length = len(sents) - train_length + 1
+test_sents = []
+# Add start and end tag
 start = (u'<s>', u'START')
 end = (u'</s>', u'END')
+for sent in sents[0: train_length]:
+    list.insert(sent, 0, start)
+    list.append(sent, end)
+    training_sents.append(sent)
 
-# Add start and end tag
 for sent in sents[test_length: len(sents)]:
     list.insert(sent, 0, start)
     list.append(sent, end)
@@ -19,5 +25,7 @@ for sent in sents[test_length: len(sents)]:
 
 
 # Get emission and transition
-# emissions = HMM.emission_probability()
-# transitions = HMM.transition_probability()
+emissions = HMM.emission_with_UNK(training_sents)
+transitions = HMM.transition_probability(training_sents)
+print len(emissions)
+print len(transitions)
