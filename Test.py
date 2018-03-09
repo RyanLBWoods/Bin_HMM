96805d92b601e62ce6054a94721ece0b389248a5
Lin Bin <linbin@8afbcfa1.cs.st-andrews.ac.uk>
2018-03-09 13:39:13 +0000

    fix viterbi

--- Test.py
+++ Test.py
@@ -9,10 +9,10 @@ print tick
 # Pre-processing training and testing sets
 sents = brown.tagged_sents(tagset='universal')  # Get training set
 # train_length = len(sents) / 2  # Define size of training set
-train_length = 5000
+train_length = 1000
 training_sents = []
 # test_length = len(sents) - train_length + 1
-test_length = len(sents) - 2
+test_length = len(sents) - 1
 testing_sents = []
 # Add start and end tag
 start = (u'<s>', u'START')
@@ -33,8 +33,8 @@ em = HMM.emission_with_unk(training_sents)
 print "em time ", time.time()
 tr = HMM.transition_probability(training_sents)
 print "time", time.time() - tick
-print len(em)
-print tr
+print em['was']
+# print tr
 
 
 def viterbi(emissions, transitions, test_sents):
@@ -42,32 +42,31 @@ def viterbi(emissions, transitions, test_sents):
     vtbs = []
     from_list = []
     maxp = 0
-    temp = ''
-    pre_tag = ''
-    last = {}
+    temp_key2 = ''
     for sent in test_sents:
         i = 0
-        print len(sent)
+        print "sent:", sent
         for token in sent:
             if i == 0:
                 if token[0] in em:
                     for key in em[token[0]]:
                         probability = em[token[0]][key] * tr[(u'START', key)]
-                        if token[0] not in vtb:
-                            vtb[(token[0], u'START')] = {}
-                            vtb[(token[0], u'START')][key] = probability
+                        if i not in vtb:
+                            vtb[i] = {}
+                            vtb[i][(u'START',key)] = probability
                         else:
-                            vtb[(token[0], u'START')][key] = probability
+                            vtb[i][(u'START',key)] = probability
                 else:
                     for key in em['UNK']:
-                        probability = em['UNK'][key] * tr[(u'START', key)]
-                        if token[0] not in vtb:
-                            vtb[(token[0], u'START')] = {}
-                            vtb[(token[0], u'START')][key] = probability
+                        if (u'START', key) in tr:
+                            probability = em['UNK'][key] * tr[(u'START', key)]
+                        if i not in vtb:
+                            vtb[i] = {}
+                            vtb[i][(u'START',key)] = probability
                         else:
-                            vtb[(token[0], u'START')][key] = probability
-                temp = (token[0], u'START')
-                pre_tag = u'START'
+                            vtb[i][(u'START',key)] = probability
+                print vtb
+                # exit(0)
             else:
                 if token[0] in em:
                     for key in em[token[0]]:
@@ -75,59 +74,72 @@ def viterbi(emissions, transitions, test_sents):
                             if tk[1] == key:
                                 temp_from = (tk[0], tk[1])
                                 from_list.append(temp_from)
-                        for key2 in from_list:
-                            if temp in vtb:
-                                if key2[0] in vtb[temp]:
-                                    observe = em[token[0]][key] * tr[key2] * vtb[temp][key2[0]]
+                        for t in vtb[i-1]:
+                            for key2 in from_list:
+                                if key2[0] == t[1]:
+                                    observe = em[token[0]][key] * tr[key2] * vtb[i-1][t]
                                     if observe > maxp:
                                         maxp = observe
-                                        pre_tag = key2[0]
-                                else:
-                                    continue
-                        if token[0] not in vtb:
-                            vtb[(token[0], pre_tag)] = {}
-                            vtb[(token[0], pre_tag)][key] = maxp
-                        else:
-                            vtb[(token[0], pre_tag)][key] = maxp
+                                        temp_key2 = key2
+                            if i not in vtb:
+                                vtb[i] = {}
+                                vtb[i][temp_key2] = maxp
+                            else:
+                                vtb[i][temp_key2] = maxp
+                        # print vtb
+                        # exit(0)
                 else:
                     for key in em['UNK']:
                         for tk in pairs:
                             if tk[1] == key:
                                 temp_from = (tk[0], tk[1])
                                 from_list.append(temp_from)
-                        for key2 in from_list:
-                            if temp in vtb:
-                                if key2[0] in vtb[temp]:
-                                    observe = em['UNK'][key] * tr[key2] * vtb[temp][key2[0]]
-                                    if observe > maxp:
+                        for t in vtb[i - 1]:
+                            for key2 in from_list:
+                                if key2[0] == t[1]:
+                                    observe = em['UNK'][key] * tr[key2] * vtb[i - 1][t]
+                                    if observe >= maxp:
                                         maxp = observe
-                                        pre_tag = key2[0]
-                                else:
-                                    continue
-                        if token[0] not in vtb:
-                            vtb[(token[0], pre_tag)] = {}
-                            vtb[(token[0], pre_tag)][key] = maxp
-                        else:
-                            vtb[(token[0], pre_tag)][key] = maxp
-            temp_token = token[0]
-            temp = (temp_token, pre_tag)
-            temp_tag = vtb[temp]
+                                        temp_key2 = key2
+                            if i not in vtb:
+                                vtb[i] = {}
+                                vtb[i][temp_key2] = maxp
+                            else:
+                                vtb[i][temp_key2] = maxp
             i += 1
             maxp = 0
             if i == len(sent):
-                last = vtb[temp]
+                # exit(0)
+                last = vtb[i - 1]
                 for tag in last:
-                    probability = last[tag] * tr[(tag, u'END')]
-                    if (u'END', tag) not in vtb:
-                        vtb[(u'END', tag)] = {}
-                        vtb[(u'END', tag)][tag] = probability
+                    probability = last[tag] * tr[(tag[1], u'END')]
+                    if i not in vtb:
+                        vtb[i] = {}
+                        vtb[i][(tag[1], u'END')] = probability
                     else:
-                        vtb[(u'END', tag)][tag] = probability
-        vtbs.append(vtb)
+                        vtb[i][(tag[1], u'END')] = probability
+            vtbs.append(vtb)
+            from_list = []
+        print vtb
         vtb = collections.OrderedDict()
-    print vtbs[0]
-    print vtbs[1]
+    return vtbs
 
 
-viterbi(em, tr, testing_sents)
+# test = viterbi(em, tr, testing_sents)
+
+def tag_sent():
+    vs = viterbi(em, tr, testing_sents)
+    tagged_sents = []
+    # temp_max = 0
+    # temp_max_tag = ''
+    # for v in vs:
+    #     for w in reversed(v):
+    #         for p_tag in v[w]:
+    #             if v[w][p_tag] > temp_max:
+    #                 temp_max = v[w][p_tag]
+    #                 temp_max_tag = p_tag
+    #
+    #                 exit(0)
+
+tag_sent()
 print time.time() - tick
